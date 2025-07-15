a0ca15282497ca5e8c89cdccfddfd349f28ca23d
Dragos Morar <pronoob1029@gmail.com>
2025-07-15 16:14:27 +0300

    moved rr

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestTele.kt
@@ -0,0 +1,11 @@
+package org.firstinspires.ftc.teamcode
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp
+
+@TeleOp
+class TestTele : LinearOpMode() {
+    override fun runOpMode() {
+        TODO("Not yet implemented")
+    }
+}
