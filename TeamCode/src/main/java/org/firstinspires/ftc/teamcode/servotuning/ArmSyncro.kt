c08723e9eed4acef398f9c281b9d3f2ff9200bd0
Dragos Morar <pronoob1029@gmail.com>
2025-07-23 14:07:45 +0300

    servo tests

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/servotuning/ArmSyncro.kt
@@ -0,0 +1,27 @@
+package org.firstinspires.ftc.teamcode.servotuning
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp
+import com.qualcomm.robotcore.hardware.Servo
+
+@TeleOp
+class ArmSync : LinearOpMode() {
+    override fun runOpMode() {
+        val leftArm = hardwareMap.get(Servo::class.java, "leftArm")
+        val rightArm = hardwareMap.get(Servo::class.java, "rightArm")
+
+        waitForStart()
+
+        while (opModeIsActive()) {
+            val leftPos = gamepad1.left_trigger.toDouble()
+            val rightPos = gamepad1.right_trigger.toDouble()
+
+            leftArm.position = leftPos
+            rightArm.position = rightPos
+
+            telemetry.addData("left pos", leftPos)
+            telemetry.addData("right pos", rightPos)
+            telemetry.update()
+        }
+    }
+}
