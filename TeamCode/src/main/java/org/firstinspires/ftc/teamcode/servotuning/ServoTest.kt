0f9a34a4d7938467e38c68017477b007618b4a98
Emi <emi.serban@gmail.com>
2025-07-23 17:43:47 +0300

    More positions

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/servotuning/ServoTest.kt
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/servotuning/ServoTest.kt
@@ -7,11 +7,16 @@ import com.qualcomm.robotcore.hardware.Servo
 @TeleOp
 class ServoTest : LinearOpMode() {
     override fun runOpMode() {
-        val servo = hardwareMap.get(Servo::class.java, "claw")
+        val servo = hardwareMap.get(Servo::class.java, "rightArm")
+        val servo2 = hardwareMap.get(Servo::class.java, "leftArm")
+        val servo3 = hardwareMap.get(Servo::class.java, "elbow")
+
+        servo2.direction = Servo.Direction.REVERSE
 
         waitForStart()
 
         var pos = 0.5
+        var pos1 = 0.5
         var currentTime = System.currentTimeMillis() / 1000.0
         var previousTime = currentTime
         var deltaTime = 0.0
@@ -21,6 +26,12 @@ class ServoTest : LinearOpMode() {
             deltaTime = currentTime - previousTime
             previousTime = currentTime
 
+            if (gamepad1.y) {
+                pos1 += deltaTime * 0.1
+            }
+            if (gamepad1.a) {
+                pos1 -= deltaTime * 0.1
+            }
             if (gamepad1.dpad_up) {
                 pos += deltaTime * 0.1
             }
@@ -29,8 +40,12 @@ class ServoTest : LinearOpMode() {
             }
 
             pos = pos.coerceIn(0.0, 1.0)
+            pos1 = pos1.coerceIn(0.0, 1.0)
             servo.position = pos
+            servo2.position = pos
+            servo3.position = pos1
             telemetry.addData("servo pos", pos)
+            telemetry.addData("servo pos", pos1)
             telemetry.update()
         }
     }
